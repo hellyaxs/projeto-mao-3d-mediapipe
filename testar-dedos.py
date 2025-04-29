97526758fb82e597a94281f56418c54d258de18e
hellyaxs <eliasvitor.dev@gmail.com>
2025-04-29 14:33:55 -0300

    chore: indicação dos pinos para posição dos dedos equivalentes

--- testar-dedos.py
+++ testar-dedos.py
@@ -3,11 +3,11 @@ import time
 
 board = Arduino('/dev/ttyUSB0')
 
-pin1 = 10
-pin2 = 9
-pin3 = 8
-pin4 = 7
-pin5 = 6
+pin1 = 10   # polegar
+pin2 = 9    # indicador
+pin3 = 8    # medio
+pin4 = 7    # anelar
+pin5 = 6    # mindinho
 
 board.digital[pin1].mode = SERVO
 board.digital[pin2].mode = SERVO
@@ -27,27 +27,28 @@ rotateServo(pin4,0)
 rotateServo(pin5,0)
 time.sleep(1)
 
-rotateServo(pin1,150)
-time.sleep(1)
-rotateServo(pin1,0)
-time.sleep(1)
+while True:
+    rotateServo(pin1,180)
+    time.sleep(1)
+    rotateServo(pin1,0)
+    time.sleep(1)
 
-rotateServo(pin2,130)
-time.sleep(1)
-rotateServo(pin2,0)
-time.sleep(1)
+    rotateServo(pin2,240)
+    time.sleep(1)
+    rotateServo(pin2,0)
+    time.sleep(1)
 
-rotateServo(pin3,130)
-time.sleep(1)
-rotateServo(pin3,0)
-time.sleep(1)
+    rotateServo(pin3,180)
+    time.sleep(1)
+    rotateServo(pin3,0)
+    time.sleep(1)
 
-rotateServo(pin4,130)
-time.sleep(1)
-rotateServo(pin4,0)
-time.sleep(1)
+    rotateServo(pin4,300)
+    time.sleep(1)
+    rotateServo(pin4,0)
+    time.sleep(1)
 
-rotateServo(pin5,130)
-time.sleep(1)
-rotateServo(pin5,0)
-time.sleep(2)
+    rotateServo(pin5,130)
+    time.sleep(1)
+    rotateServo(pin5,0)
+    time.sleep(2)
